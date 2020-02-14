34c72c6aaa757e83552bdc9fd3dc790bc9d8ca19
Todd Norton <toddmnorton@live.com>
2020-02-14 22:00:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 test_string = "This is my test string for this lab." 
 
 def reverse_each_word(sentence)
-  puts sentence.split.collect 
+  sentence.split.collect 
 end
 
 puts reverse_each_word(test_string)
